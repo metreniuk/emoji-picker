20c4b84347b52b577fe514746624437b24ddb551
Alexandr Metreniuc <metreniuk@users.noreply.github.com>
2019-09-28 17:00:18 +0300

    Implement search functionality

--- src/EmojiPicker/components/Search/Search.js
+++ src/EmojiPicker/components/Search/Search.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import classNames from "classnames"
 import "./Search.css"
 
-const Search = ({ className }) => (
-  <div className={classNames("search", className)} />
-)
+import * as Svg from "../Svg"
+
+const useInput = initialState => {
+  const [value, setValue] = useState(initialState)
+
+  return {
+    value,
+    setValue: e => setValue(e.target.value),
+    reset: () => setValue(initialState)
+  }
+}
+
+const Search = ({ className }) => {
+  const { value, setValue, reset } = useInput("")
+
+  return (
+    <div className={classNames("search", className)}>
+      <Svg.Search className="search__search-icon" />
+      <input
+        className="search__input"
+        placeholder="Search"
+        value={value}
+        onChange={setValue}
+      />
+      {Boolean(value) && (
+        <button className="search__close-btn" onClick={reset}>
+          <Svg.Close className="search__close-icon" />
+        </button>
+      )}
+    </div>
+  )
+}
 
 export default Search
